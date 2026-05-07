00555092249199233a2efe5028387d2125482baa
ziethoag-sudo <ziethoag@gmail.com>
2026-05-08 00:44:50 +0700

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Yêu Gok Nhất Luôn Á",
         // Activity type number (0 = Playing).
         type: 0, 
       },
