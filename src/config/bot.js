b1a4816babf5935e20a121ccab0ab6fc8198f3e2
ziethoag-sudo <ziethoag@gmail.com>
2026-05-08 00:46:39 +0700

    Change bot activity type from Playing to Streaming

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Yêu Gok Nhất Luôn Á",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
